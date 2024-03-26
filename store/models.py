e6e90484041228b5e7b12d3855e1a71e74933355
lawrence <lawcubegsm@gmail.com>
2024-03-26 12:44:10 +0000

    Add color model to product

--- store/models.py
+++ store/models.py
@@ -6,6 +6,11 @@ from custom_users.models import User
 # Create your models here.
 
 
+class Color:
+    name = models.CharField(max_length=15,  unique=True)
+    code = models.CharField(max_length=15, unique=True)
+
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length='120', blank=False, unique=True)
@@ -13,16 +18,20 @@ class Product(models.Model):
     description = models.CharField(max_length=255, blank=False , null=False)
     created_at = models.DateTimeField( auto_now_add=True, default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    cost_price = models.DecimalField(_(""), max_digits=5, decimal_places=2, blank=False, null=False)
-    saling_price = models.DecimalField(_(""), max_digits=5, decimal_places=2, blank=False, null=False)
-
+    quantity= models.IntegerField()
+    cost_price = models.DecimalField(_("cp"), max_digits=5, decimal_places=2, blank=False, null=False)
+    saling_price = models.DecimalField(_("sp"), max_digits=5, decimal_places=2, blank=False, null=False)
+    color = models.ManyToManyField(Color)
     class Meta:
         verbose_name = _("product")
         verbose_name_plural = _("products")
 
     def __str__(self) -> str:
         return self.name
-        
+
+
+
+
 
         
 
