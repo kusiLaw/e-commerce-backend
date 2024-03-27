cfd787316f7dceb0d676af4834eb108b1feff21b
lawrence <lawcubegsm@gmail.com>
2024-03-27 14:28:56 +0000

    Add products image model
    - add product model indexing

--- store/models.py
+++ store/models.py
@@ -12,29 +12,45 @@ class Color(models.Model):
 
 class Size(models.Model):
     text = models.CharField(max_length=15,  unique=True)
-    unit = models.CharField(max_lenth=15, null=True, blank=True,
+    unit = models.CharField(max_length=15, null=True, blank=True,
                             help_text='Handle at frontend since unit is not known ahead of time')
 
-
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    name = models.CharField(max_length='120', blank=False, unique=True)
+    Tag = [
+        ("new", "new"),
+        ("hot", "hot"),
+        ("sales", "sales"),
+    ]
+    user = models.ForeignKey(User, verbose_name= _('product_owner'), on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=120, blank=False, unique=True)
     product_code = models.CharField(max_length=100, blank=True, null=True)
     description = models.CharField(max_length=255, blank=False , null=False)
-    created_at = models.DateTimeField( auto_now_add=True, default=timezone.now)
+    created_at = models.DateTimeField( auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     quantity= models.IntegerField()
+    tag = models.CharField(max_length=5, choices=Tag)
     cost_price = models.DecimalField(_("cp"), max_digits=5, decimal_places=2, blank=False, null=False)
     saling_price = models.DecimalField(_("sp"), max_digits=5, decimal_places=2, blank=False, null=False)
     color = models.ManyToManyField(Color)
     size = models.ManyToManyField(Size)
+
+
     class Meta:
         verbose_name = _("product")
         verbose_name_plural = _("products")
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
 
     def __str__(self) -> str:
         return self.name
 
+
+
+class Image(models.Model):
+    image = image = models.ImageField(
+        max_length=100, upload_to='store/static/')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
 
