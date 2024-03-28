0725b70c78751ae12bc7846ec19b0990600595a2
lawrence <lawcubegsm@gmail.com>
2024-03-28 12:52:12 +0000

    Add models serializer

---
+++ store/api/v1/serializer.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from store.models import ProductImage, Product, ProductColor, ProductSize
+
+
+class ProductImageSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = ProductImage
+        fields = ['image',]
+
+
+class ProductSizesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductSize
+        fields = ['text', 'unit']
+
+
+class ProductColorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductColor
+        fields = ['name', 'code']
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    color = ProductColorSerializer(many=True, read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail', read_only=True)
+
+    product_image = ProductImageSerializer(
+        many=True,
+        read_only=True,
+    )
+
+
+    class Meta:
+        model = Product
+        fields = [ 'id', 'url','name', 'product_code',
+                  'description', 
+                  'tag', 'saling_price', 'color', 'product_image']
+        # depth = 2
+      
+
+class ProductDetailSerializer(serializers.HyperlinkedModelSerializer):
+    size = ProductSizesSerializer(many=True, read_only=True)
+    color = ProductColorSerializer(many=True, read_only=True)
+    product_image = ProductImageSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'url', 'name', 'product_code',
+                  'description', 'created_at', 'quantity',
+                  'tag', 'saling_price', 'color', 'size', 'product_image']
