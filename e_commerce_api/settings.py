52ec36da1a65e161e4b9893732c240bee066cb1c
lawrence <lawcubegsm@gmail.com>
2024-07-28 23:26:43 +0100

    Add MySql settings to deploy

--- e_commerce_api/settings.py
+++ e_commerce_api/settings.py
@@ -98,13 +98,24 @@ WSGI_APPLICATION = 'e_commerce_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": getenv('DB_NAME'),
+        "USER": getenv('DB_USER'),
+        "PASSWORD": getenv('DB_PASSWORD'),
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
